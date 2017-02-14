0fc2a7bcd5c383bfb38bc6fa6a9a35b5ea807a6f
Thomas <thomasd7@lancs.local>
2017-02-14 12:10:39 +0000

    Added instance variables and constructor

--- Snake.java
+++ Snake.java
@@ -1,8 +1,24 @@
 public class Snake
 {
+	//Instance Variables
+	private double xPosition;
+	private double yPosition;
+	private double length;   //---->
+	private double speed;    //---->
+	private String colour;
+	
+	private Rectangle snakeHead = new Rectangle(xPosition, yPosition, 20, 20, colour);
 
-
-
-
-
+	//Constructors
+	public Snake(double x, double y)
+	{
+		xPosition = x;
+		yPosition = y;
+		colour = "GREEN";
+	}
+	
+	public void addTo(GameArena arenaName)
+	{
+	
+	}
 }
